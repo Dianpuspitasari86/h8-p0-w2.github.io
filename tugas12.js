807c15af1044cb82e0c8fe51dd7fdee60386ed10
Dian Puspita Sari <dianpuspitasari171586@gmail.com>
2019-08-01 18:55:21 +0700

    tugas 12 js

--- tugas12.js
+++ tugas12.js
@@ -1,7 +1,11 @@
 function konversimenit(menit) {
-    return 
+    var h = Math.floor(menit / 60);
+    var m = (menit % 60).toString().length === 1 ? "0" + Math.floor(menit % 60) : Math.floor(menit % 60) ;
+    return h + " : " + m;
 }
 
 console.log(konversimenit(63));
-console.log(konversimenit(124));console.log(konversimenit(53));console.log(konversimenit(88));
+console.log(konversimenit(124));
+console.log(konversimenit(53));
+console.log(konversimenit(88));
 console.log(konversimenit(120));
